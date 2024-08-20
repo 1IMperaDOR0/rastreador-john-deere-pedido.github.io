beba98b23b5ecb111d4ba52762cd406e6e2acb68
1MperaDOR0 <lucaslucore@gmail.com>
2024-08-20 13:14:13 -0300

    configuring orders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
-let menu = document.querySelector(".menu")
+const q = (el) => document.querySelector(el)
+const qa = (el) => document.querySelectorAll(el)
+
+let modalQt = 1
+
+let menu = q(".menu")
 menu.addEventListener("click", () => {
-    let nav = document.querySelector('nav')
+    let nav = q('nav')
     nav.style.display = 'block'
     menu.style.display = 'none'
 })
 
-let close = document.querySelector(".close")
+let close = q(".close")
 close.addEventListener("click", () => {
-    let nav = document.querySelector('nav')
+    let nav = q('nav')
     nav.style.display = 'none'
     menu.style.display = 'block'
+})
+
+q('.kitInfo--qtmais.p').addEventListener('click', () => {
+    modalQt++
+    q('.kitInfo--qt.p').innerHTML = modalQt
+})
+q('.kitInfo--qtmenos.p').addEventListener('click', () => {
+    if(modalQt > 1){
+        modalQt--
+        q('.kitInfo--qt.p').innerHTML = modalQt
+    }
+})
+qa('.kitInfo--size.p').forEach((size, sizeIndex) => {
+    size.addEventListener('click', () => {
+        q('.kitInfo--size.p.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
 })
